c915e39f4646a26b488664da37cf6cfcd7ac3b9c
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-11 20:13:38 +0530

    Create LuckySeven.cpp

---
+++ Level 0/LuckySeven.cpp
@@ -0,0 +1,12 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string S;
+    cin >> S;
+    cout << S[6];
+}
