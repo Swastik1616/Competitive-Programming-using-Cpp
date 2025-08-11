bfa950c14093cad24a874fefaa860463913582ca
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-11 20:13:35 +0530

    Create CricketWorldCupQualifier.cpp

---
+++ Level 0/CricketWorldCupQualifier.cpp
@@ -0,0 +1,13 @@
+/* Problem Link - 
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int score;
+    cin >> score;
+    if(score>=12) cout << "Yes";
+    else cout << "No";
+}
