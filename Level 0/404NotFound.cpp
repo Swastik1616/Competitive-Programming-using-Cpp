99db9e46b1054175e8827472fc27e5b46c0b5660
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-12 16:52:59 +0530

    Progress
    Radhe Radhe
    12 August 2025

---
+++ Level 0/404NotFound.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin >> x;
+	if(x==404) cout << "NOT FOUND";
+	else cout << "FOUND";
+}
