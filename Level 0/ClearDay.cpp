d19c0070724189082e4ac5c67da03760a7a5fabd
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-12 16:53:20 +0530

    Create ClearDay.cpp
    Radhe Radhe
    12 August 2025

---
+++ Level 0/ClearDay.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int rainy, cloudy;
+    cin >> rainy >> cloudy;
+    cout << 7 - (rainy + cloudy);
+}   
