cda0659f17ca381fbc43a704cc62c32062ced721
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-12 16:53:52 +0530

    Create OffByOne.cpp
    Radhe Radhe
    12 August 2025

---
+++ Level 0/OffByOne.cpp
@@ -0,0 +1,12 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << "1";
+}
