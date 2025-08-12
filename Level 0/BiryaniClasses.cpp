b84e05e408442ac1e5e3562afdd8c200b93893dc
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-12 16:53:10 +0530

    Create BiryaniClasses.cpp
    Radhe Radhe
    12 August 2025

---
+++ Level 0/BiryaniClasses.cpp
@@ -0,0 +1,16 @@
+/* Problem Link - 
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << x*y << endl;
+    }
+}   
