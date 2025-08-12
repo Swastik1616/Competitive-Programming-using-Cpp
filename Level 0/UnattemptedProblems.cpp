68b2dbb2396fa7069c5a8936c79b4241137bb5ac
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-12 16:54:05 +0530

    Create UnattemptedProblems.cpp
    Radhe Radhe
    12 August 2025

---
+++ Level 0/UnattemptedProblems.cpp
@@ -0,0 +1,12 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin >> x >> y;
+    cout << x-y;
+}
