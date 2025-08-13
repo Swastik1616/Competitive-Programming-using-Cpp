73fe3b0824d4c2ed8a25208aa01b8a0326b9575b
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-13 19:15:11 +0530

    Create 2000.cpp
    13 August 2025
    Radhe Radhe

---
+++ Level 0/2000.cpp
@@ -0,0 +1,12 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin >> n;
+    cout << 4*n;
+}
