95d119bc02ea7a76efd6b1d5ea3c6b80ee27472f
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-13 19:15:30 +0530

    Create PuzzleHunt.cpp
    13 August 2025
    Radhe Radhe

---
+++ Level 0/PuzzleHunt.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if(x>=6 && x<=8) cout << "YES";
+    else cout << "NO";
+}
