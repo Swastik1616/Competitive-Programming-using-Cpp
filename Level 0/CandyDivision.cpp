b777a7f494896a4715d9b154b0902ae7e5d50d75
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-13 19:15:26 +0530

    Create CandyDivision.cpp
    13 August 2025
    Radhe Radhe

---
+++ Level 0/CandyDivision.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CANDIVIDE
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if (n%3==0) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
