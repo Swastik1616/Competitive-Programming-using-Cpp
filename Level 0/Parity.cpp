bb762ec6144f41c104b316023e570db722c30a16
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-14 20:11:57 +0530

    Create Parity.cpp
    14 August 2025
    Radhe Radhe

---
+++ Level 0/Parity.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x%2==0) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
