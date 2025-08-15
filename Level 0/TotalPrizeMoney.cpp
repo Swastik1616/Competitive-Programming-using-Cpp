2e4a139a8e8544aa6445c7423656af3a5ec73668
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-15 21:58:34 +0530

    Create TotalPrizeMoney.cpp
    15 August 2025
    Radhe Radhe

---
+++ Level 0/TotalPrizeMoney.cpp
@@ -0,0 +1,16 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRIZEPOOL
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << (10*x)+(90*y) << endl;
+    }
+}
