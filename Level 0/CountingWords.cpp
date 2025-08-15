07502bf0384e90fed7b2a5f5ecf58e630c1db7fe
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-15 22:04:29 +0530

    Create CountingWords.cpp
    15 August 2025
    Radhe Radhe

---
+++ Level 0/CountingWords.cpp
@@ -0,0 +1,16 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        cout << n*m << endl;
+    }
+}
