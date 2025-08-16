7686f04c4c48befd057649e81a774b48ef1a9ecc
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-16 13:54:39 +0530

    Create TomAndJeryChase.cpp
    16 August 2025
    Radhe Radhe

---
+++ Level 0/TomAndJeryChase.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        if(y>x) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
