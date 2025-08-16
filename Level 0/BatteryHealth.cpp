68d83102bc0c6505fa064f327405a534a4048353
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-16 13:54:35 +0530

    Create BatteryHealth.cpp
    16 August 2025
    Radhe Radhe

---
+++ Level 0/BatteryHealth.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BTRYHLTH
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x>=80) cout << "YES\n";
+        else cout << "NO\n";
+    } 
+}
