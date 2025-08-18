fa930eb9facd18403c7514a5c610074bb581df66
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-18 19:36:09 +0530

    Create SumIt.cpp

---
+++ Level 0/SumIt.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SUMM
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here  
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b,c;
+	    cin >> a >> b >> c;
+	    if(a+b==c) cout << "YES\n";
+	    else cout << "NO\n";
+	}
+}
