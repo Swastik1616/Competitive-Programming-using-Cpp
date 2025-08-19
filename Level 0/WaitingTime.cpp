940014b5b04f14d87d54a96cae52d918ac42a835
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-19 22:21:48 +0530

    Create WaitingTime.cpp
    Radhe Radhe
    19 August 2025

---
+++ Level 0/WaitingTime.cpp
@@ -0,0 +1,16 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WAITTIME
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int k,x;
+        cin >> k >> x;
+        cout << 7*k - x << endl;
+    }
+}
