f5e9110ac13778d2d718d32746f2273a2a8df8cc
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-20 19:58:51 +0530

    Create JustOneMoreEpisode.cpp
    20 August 2025
    Radhe Radhe

---
+++ Level 0/JustOneMoreEpisode.cpp
@@ -0,0 +1,16 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ONEMORE
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x>24) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}   
