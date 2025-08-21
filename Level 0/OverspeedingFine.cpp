d657287bb93f8b11860a6a644d23a76290012e62
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-21 21:44:21 +0530

    Create OverspeedingFine.cpp
    21 August 2025
    Radhe Radhe

---
+++ Level 0/OverspeedingFine.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FINE
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x<=70) cout << "0\n";
+        else if(x<=100) cout << "500\n";
+        else cout << "2000\n";
+    }
+}
