ec9da70035b143eb6d22c40daea1e8a3d8d2c105
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-22 19:48:03 +0530

    Update CredCoins.cpp
    Radhe Radhe
    22 August 2025

---
+++ Level 1/CredCoins.cpp
@@ -0,0 +1,16 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CREDCOINS
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << (x*y)/100 << endl;
+    }
+}
