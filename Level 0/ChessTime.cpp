98cdadace3f335fe882213cb18b961526b1b15fa
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-22 19:47:59 +0530

    Create ChessTime.cpp
    Radhe Radhe
    22 August 2025

---
+++ Level 0/ChessTime.cpp
@@ -0,0 +1,16 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHESSTIME
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << n*3 << endl; // (n*60)/20;
+    }
+}
