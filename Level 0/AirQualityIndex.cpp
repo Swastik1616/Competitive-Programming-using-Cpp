88dc05660c010894794ceea1ebe0c997420efaa8
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-25 19:50:42 +0530

    Create AirQualityIndex.cpp
    25 August 2025
    Radhe Radhe

---
+++ Level 0/AirQualityIndex.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if (x<100) cout << "YES\n";
+    else cout << "NO\n";
+}
