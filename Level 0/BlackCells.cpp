61befbb221298bb585a7d6a5420744c2d982c135
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-26 20:04:27 +0530

    Create BlackCells.cpp
    26 August 2025
    Radhe Radhe

---
+++ Level 0/BlackCells.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/cpp/PCPP04/problems/BLACKCEL
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+	// your code goes here
+	cout << (n*n)/2 << endl;
+	return 0;
+}
