073450571ec2649541639faaa829238fd859ccc7
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-26 20:00:22 +0530

    Create FirstContest.cpp
    26 August 2025
    Radhe Radhe

---
+++ Level 0/FirstContest.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/cpp/PCPP04/problems/MY1STCONTEST
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a,b;
+    cin>>n>>a>>b;
+	// your code goes here
+	cout << n - a << " " << (n-a)-b << endl;
+	return 0;
+}
