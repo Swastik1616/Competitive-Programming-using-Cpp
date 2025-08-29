989577e4c3f4ec622a16ee17fb6d94e9ab77f85d
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-29 21:16:39 +0530

    Create WaterRequirement.cpp
    29 August 2025
    Radhe Radhe

---
+++ Level 0/WaterRequirement.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << n*2 << endl;
+    }
+}
+s
