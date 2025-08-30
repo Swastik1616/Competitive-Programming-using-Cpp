1a3c51c22e800c8509d2b3815d8fc3981a3f145a
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-30 23:44:53 +0530

    Create Lunchtime.cpp
    30 August 2025
    Radhe Radhe

---
+++ Level 0/Lunchtime.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x>=1 && x<=4) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
