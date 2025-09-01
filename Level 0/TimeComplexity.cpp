6ab4e87c9f04e5fcf6c29eb56cefcefc4b6f0965
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-01 20:46:20 +0530

    Create TimeComplexity.cpp
    Radhe Radhe
    1 September 2025

---
+++ Level 0/TimeComplexity.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COMPLEXITY
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        if(x>y) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
