a6f18fe5ed339bb092597f5c253e1fe42f83d650
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-01 20:38:21 +0530

    Update ChefGivesParty.cpp
    Radhe Radhe
    1 September 2025

--- Level 0/ChefGivesParty.cpp
+++ Level 0/ChefGivesParty.cpp
@@ -1,3 +1,18 @@
 /* Problem Link -
 https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARTY2
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,x,k;
+        cin >> n >> x >> k;
+        if((n*x)<=k) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
