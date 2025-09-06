6810e82271e998366200356fb543572b0a805402
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-06 22:18:04 +0530

    Create MinimumCoins.cpp
    6 September 2025
    Radhe Radhe

---
+++ Level 0/MinimumCoins.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        int rem = x % 10;
+        cout << rem << endl;
+    }
+}
