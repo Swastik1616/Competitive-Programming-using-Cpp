3434386df36f9b9eaa13758a3a9b5a954c2f0c0e
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-10 14:28:55 +0530

    Update ProfitIncrement.cpp
    10 September 2025
    Radhe Radhe

--- Level 0/ProfitIncrement.cpp
+++ Level 0/ProfitIncrement.cpp
@@ -1,3 +1,17 @@
 /* Problem Link -
-
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PROINC
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << (x + (0.1*x)) - (x-y) << endl;
+    }   
+}
