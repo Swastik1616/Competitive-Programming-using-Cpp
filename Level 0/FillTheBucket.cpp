1a889e7de2b4914c96f24da55dc4f9654299907f
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-10 14:28:43 +0530

    Create FillTheBucket.cpp
    10 September 2025
    Radhe Radhe

---
+++ Level 0/FillTheBucket.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FBC
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int k,x;
+        cin >> k >> x;
+        cout << k - x << endl;
+    }
+}
