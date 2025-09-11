777969216ea2624726f73c0d3d6d1675cc232962
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-11 17:51:23 +0530

    Create ChefAndMasks.cpp
    11 September 2025
    Radhe Radhe

---
+++ Level 0/ChefAndMasks.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CMASKS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        if((100*x) < (10*y)) cout << "DISPOSABLE\n";
+        else cout << "CLOTH\n";
+    }
+}
