cea212f5ac481074661101a2a0d0b63027d79474
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-16 21:17:19 +0530

    Create CountTheNotebooks.cpp
    16 September 2025
    Radhe Radhe

---
+++ Level 1/CountTheNotebooks.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NOTEBOOK
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << n*10 << endl;
+    }
+}
