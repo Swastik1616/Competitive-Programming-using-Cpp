6b8c5ca617564d02a74be1d035798eb49dd7b5cd
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-16 21:17:16 +0530

    Create ChefAndNextGen.cpp
    16 September 2025
    Radhe Radhe

---
+++ Level 1/ChefAndNextGen.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/HELIUM3
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,x,y;
+        cin >> a >> b >> x >> y;
+        if(a*b<=x*y) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
