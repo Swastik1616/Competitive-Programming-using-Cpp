ff574e5cfaf0e2cd716a8c085325519a936c7b38
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-16 21:17:13 +0530

    Create TypeProblem.cpp
    16 September 2025
    Radhe Radhe

---
+++ Level 0/TypeProblem.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TYRE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        cout << (n*2) + (m*4) << endl;
+    }
+}
