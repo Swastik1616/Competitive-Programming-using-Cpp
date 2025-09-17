27e8874effa4f573fe702e22eff7f8e30bcf61cb
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-17 22:26:20 +0530

    Create SumOfDigits.cpp
    17 September 2025
    Radhe Radhe

---
+++ Level 0/SumOfDigits.cpp
@@ -0,0 +1,22 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW006
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int sum = 0;
+        while(n!=0){
+            sum+=n%10;
+            n/=10;
+        }
+        cout << sum << endl;
+    }
+}
