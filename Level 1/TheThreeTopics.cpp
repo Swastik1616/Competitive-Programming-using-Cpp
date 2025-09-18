3ec26c4be0e9349f18a72cc8b20ed51594c83b64
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-18 16:50:05 +0530

    Create TheThreeTopics.cpp
    18 September 2025
    Radhe Radhe

---
+++ Level 1/TheThreeTopics.cpp
@@ -0,0 +1,14 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/THREETOPICS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b,c,x;
+    cin >> a >> b >> c >> x;
+    if(x==a || x==b || x==c) cout << "YES\n";
+    else cout << "NO\n";
+}
