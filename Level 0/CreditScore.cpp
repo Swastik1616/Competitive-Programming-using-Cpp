b0fb458b45294bc5898506eac44f5709fa5c1e76
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-19 21:30:38 +0530

    Create CreditScore.cpp
    19 September 2025
    Radhe Radhe

---
+++ Level 0/CreditScore.cpp
@@ -0,0 +1,14 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CREDSCORE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if(x>=750) cout << "YES\n";
+    else cout << "NO\n";
+}
