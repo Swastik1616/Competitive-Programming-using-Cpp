2f49ca9778dba87b62d294d913d1aa3280fbcb8c
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-20 20:37:37 +0530

    Create NearestExit.cpp
    20 September 2025
    Radhe Radhe

---
+++ Level 1/NearestExit.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NEARESTEXIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x>50) cout << "RIGHT\n";
+        else cout << "LEFT\n";
+    }
+}
