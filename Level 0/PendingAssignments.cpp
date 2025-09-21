1d35e319c7c294ec0a3811a01b833afe14363dd8
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-21 21:24:38 +0530

    Create PendingAssignments.cpp
    21 September 2025
    Radhe Radhe

---
+++ Level 0/PendingAssignments.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ASSIGNMNT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y,z;
+        cin >> x >> y >> z;
+        if(x*y <= z*24*60) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
