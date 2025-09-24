925d63614db9d0dd26c917e0ca2ddcc3a0001304
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-24 22:57:03 +0530

    Create IncreaseIQ.cpp
    24 September 2025
    Radhe Radhe

---
+++ Level 0/IncreaseIQ.cpp
@@ -0,0 +1,14 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INCRIQ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if(x+7>170) cout << "YES\n";
+    else cout << "NO\n";
+}
