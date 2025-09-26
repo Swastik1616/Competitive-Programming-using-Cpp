e274678a3290b5936e8ed2c7350f4c6f9fd49b7b
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-26 19:30:29 +0530

    Create GoodProgram.cpp
    26 September 2025
    Radhe Radhe

---
+++ Level 1/GoodProgram.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NIBBLE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n%4 == 0) cout << "Good\n";
+        else cout << "Not Good\n";
+    }
+}
