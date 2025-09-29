15a489eab704d65614052ecfdc0dc5d0461888e8
Swastik555 <swastikgondhi2204@gmail.com>
2025-09-29 22:52:36 +0530

    Create BatteryLow.cpp
    29 September 2025
    Radhe Radhe

---
+++ Level 0/BatteryLow.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BATTERYLOW
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x<=15) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
