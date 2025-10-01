f56f61e6ae8eb9c7d4f89af8dbbccf8ca9e64198
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-01 19:45:24 +0530

    Update TestScore.cpp
    1 October 2025
    Radhe Radhe

--- Level 1/TestScore.cpp
+++ Level 1/TestScore.cpp
@@ -1,3 +1,27 @@
 /* Problem Link -
-
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFSCORE
+IMPORTANT QUESTION
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,x,y;
+        cin >> n >> x >> y;
+        if(y==0) cout << "YES\n";
+        else {
+                int count = 0;
+                for(int i=1; i<=n; i++){
+                    if((y % (x*i)) == 0) count++;
+                }
+        
+                if(count == 0 || (n==1 && x<y) || (n*x < y)) cout << "NO\n";
+                else cout << "YES\n";
+        }
+    }
+}
