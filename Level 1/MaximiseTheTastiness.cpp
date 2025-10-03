13035f0c7dbff335dbd45b3599b01a53c3bfff84
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-03 19:32:39 +0530

    Create MaximiseTheTastiness.cpp
    3 October 2025
    Radhe Radhe

---
+++ Level 1/MaximiseTheTastiness.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MAXTASTE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c,d;
+        cin >> a >> b >> c >> d;
+        cout << max(max(a+c,a+d),max(b+c,b+d)) << endl;
+    }
+}
