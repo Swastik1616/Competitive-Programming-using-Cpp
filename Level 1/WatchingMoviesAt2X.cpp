2bf75444629889df03176e1b46b80acd1438b7c3
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-03 19:32:42 +0530

    Create WatchingMoviesAt2X.cpp
    3 October 2025
    Radhe Radhe

---
+++ Level 1/WatchingMoviesAt2X.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin >> x >> y;
+    cout << y/2 + (x-y) << endl;
+}
