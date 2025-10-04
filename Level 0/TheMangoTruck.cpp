77f0a91c462aa93d4b253996ffb9cdbd06fdc037
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-04 14:43:06 +0530

    Create TheMangoTruck.cpp
    4 October 2025
    Radhe Radhe

---
+++ Level 0/TheMangoTruck.cpp
@@ -0,0 +1,17 @@
+/* Problem Link - 
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANGOES
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y,z;
+        cin >> x >> y >> z;
+        cout << (z-y)/x << endl;
+    }
+}
