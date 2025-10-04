ffbed7826a68e271de9477611b9b2d147103dd7f
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-04 14:43:09 +0530

    Create BathInWinters.cpp
    4 October 2025
    Radhe Radhe

---
+++ Level 1/BathInWinters.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/BATH
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << x/(2*y) << endl;
+    }
+}
