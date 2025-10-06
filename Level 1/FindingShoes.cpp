e34c63c4530cecc1b6d6d01b58a290588c51970b
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-06 22:34:14 +0530

    Update FindingShoes.cpp
    6 October 2025
    Radhe Radhe

--- Level 1/FindingShoes.cpp
+++ Level 1/FindingShoes.cpp
@@ -2,3 +2,19 @@
 https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FINDSHOES
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        if(n==m) cout << n << endl;
+        else if(n>m) cout << 2*(n-m) + m << endl;
+        else cout << n << endl;
+        
+    }
+}   
