0026e6f6322706f67049132ab7ceff4e4db2f9a6
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-06 22:34:06 +0530

    Update BucketAndWaterFlow.cpp
    6 October 2025
    Radhe Radhe

--- Level 0/BucketAndWaterFlow.cpp
+++ Level 0/BucketAndWaterFlow.cpp
@@ -2,3 +2,18 @@
 https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERFLOW
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int w,x,y,z;
+        cin >> w >> x >> y >> z;
+        if(y*z > x-w) cout << "overflow\n";
+        else if(y*z < x-w) cout << "unfilled\n";
+        else cout << "filled\n";
+    }
+}
