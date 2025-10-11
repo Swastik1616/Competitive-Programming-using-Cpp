cabf14bce682742796f08bb4e4ce1a3c743ba69c
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-11 19:15:56 +0530

    Update ChefAndGym.cpp
    11 October 2025
    Radhe Radhe

--- Level 0/ChefAndGym.cpp
+++ Level 0/ChefAndGym.cpp
@@ -2,3 +2,18 @@
 https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CGYM
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y,z;
+        cin >> x >> y >> z;
+        if(z>=x && z<x+y) cout << "1\n";
+        else if(z>=x+y) cout << "2\n";
+        else cout << "0\n";
+    }
+}
