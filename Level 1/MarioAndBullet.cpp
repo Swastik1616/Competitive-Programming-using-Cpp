b9877c7bb8c5033639c951a1826b5dd78e58031a
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-13 22:44:27 +0530

    Update MarioAndBullet.cpp
    13 October 2025
    Radhe Radhe

--- Level 1/MarioAndBullet.cpp
+++ Level 1/MarioAndBullet.cpp
@@ -2,3 +2,17 @@
 https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/BULLET
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y,z;
+        cin >> x >> y >> z;
+        if(y/x < z) cout << z - y/x << endl;
+        else cout << 0 << endl;
+    }
+}
