7a67e212c1d4c7097510cb069cb4ee4d42b043d5
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-15 21:50:09 +0530

    Create SpringCleaning.cpp
    15 October 2025
    Radhe Radhe

---
+++ Contests/15-October-2025/SpringCleaning.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/START208D/problems/SPRCLN
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin >> x >> y;
+    cout << x*30 + y*60 << endl;
+}
