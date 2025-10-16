266921334ee09ab0d0e57409c538f9fd49005e1c
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-16 19:07:16 +0530

    Create FindingSquareRoots.cpp
    16 October 2025
    Radhe Radhe

---
+++ Level 1/FindingSquareRoots.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FSQRT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << floor(sqrt(n)) << endl;
+    }
+}
