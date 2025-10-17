83494d18de9a13ba17882994fe3c6bb33552bf2e
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-17 21:40:42 +0530

    Create ValentineIsComing.cpp
    17 October 2025
    Radhe Radhe

---
+++ Level 1/ValentineIsComing.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/VALENTINE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << x/y << endl;
+    }
+}   
