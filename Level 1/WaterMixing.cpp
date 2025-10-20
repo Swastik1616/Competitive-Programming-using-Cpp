278d2ae2774a90d1db580feec078ee091ea25279
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-20 22:16:35 +0530

    Update WaterMixing.cpp
    20 October 2025
    Radhe Radhe

--- Level 1/WaterMixing.cpp
+++ Level 1/WaterMixing.cpp
@@ -2,3 +2,19 @@
 https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/WTRMIXING
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,x,y;
+        cin >> a >> b >> x >> y;
+        if(a<b && (b-a)<=x)  cout << "YES\n";
+        else if(b<a && (a-b)<=y) cout << "YES\n";
+        else if(a==b) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
