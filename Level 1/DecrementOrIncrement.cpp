ad3819b11b940b9c368bfd3bc3930c7a61ad905e
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-23 19:47:29 +0530

    Create DecrementOrIncrement.cpp
    23 October 2025
    Radhe Radhe

---
+++ Level 1/DecrementOrIncrement.cpp
@@ -0,0 +1,14 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/DECINC
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin >> n;
+    if(n%4 == 0) cout << n+1 << endl;
+    else cout << n-1 << endl;
+}
