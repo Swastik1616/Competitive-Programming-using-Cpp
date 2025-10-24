2b658245a17da4b5b36163dda26cb2b3e4745e6f
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-24 16:48:31 +0530

    Create TooManyItems.cpp
    24 October 2025
    Radhe Radhe

---
+++ Level 1/TooManyItems.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/POLYBAGS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << ceil(n/10.0) << endl;
+    }
+}
