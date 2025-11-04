8855a48a974670c039c735b6a5cc0434d144820b
Swastik555 <swastikgondhi2204@gmail.com>
2025-11-04 22:54:30 +0530

    Update MinimumNumberOfFlips.cpp
    4 November 2025
    Radhe Radhe

--- CodeChef/Level 1/MinimumNumberOfFlips.cpp
+++ CodeChef/Level 1/MinimumNumberOfFlips.cpp
@@ -2,3 +2,33 @@
 https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MINFLIPS
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> v;
+        int c1=0;
+        int c2=0;
+        for(int i=0;i<n;i++){
+            int x;
+            cin >> x;
+            v.push_back(x);
+            if(v[i] == 1) c1++;
+            else c2++;
+        }
+        int dif = abs(c1-c2);
+        if(n%2==0){
+            if(c1==c2) cout << "0\n";
+            else if((dif == 2)) cout << "1\n";
+            else if(c1==n || c2==n) cout << n/2 << endl;
+            else if((dif > 2)) cout << dif/2 << endl;
+        }
+        else cout << "-1\n";
+    }
+}
