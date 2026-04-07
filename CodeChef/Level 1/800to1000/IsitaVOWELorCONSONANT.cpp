500c6ef421a740c23274d24b294235e68cb02f2a
Swastik555 <swastikgondhi2204@gmail.com>
2026-04-07 20:50:11 +0530

    Create IsitaVOWELorCONSONANT.cpp

---
+++ CodeChef/Level 1/800to1000/IsitaVOWELorCONSONANT.cpp
@@ -0,0 +1,15 @@
+// Problem Link - https://www.codechef.com/practice/course/logical-problems/DIFF1000/problems/VOWELTB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	char c;
+	cin >> c;
+	
+	if(c=='A' || c=='O' || c=='I' || c=='E' || c=='U')
+	cout << "Vowel\n";
+	else cout << "Consonant\n";
+
+}
