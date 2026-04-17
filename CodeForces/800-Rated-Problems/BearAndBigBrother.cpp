4a19ce7f9bcde4e47c5372546e8e752cbe05c8b4
Swastik555 <swastikgondhi2204@gmail.com>
2026-04-17 15:50:51 +0530

    Create BearAndBigBrother.cpp

---
+++ CodeForces/800-Rated-Problems/BearAndBigBrother.cpp
@@ -0,0 +1,16 @@
+// Problem Link - https://codeforces.com/problemset/problem/791/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,ans=0;
+    cin >> a >> b;
+    while(a<=b){
+        a*=3;
+        b*=2;
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
