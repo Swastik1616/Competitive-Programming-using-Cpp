532542265807a810cba2004316d28d0f8af53e1b
Swastik555 <swastikgondhi2204@gmail.com>
2026-04-18 19:22:47 +0530

    Create A. ClosedInterval.cpp

---
+++ AtCoder/Contests/18-April-2026-Keysight Technologies Programming Contest（AtCoder Beginner Contest 454）/A. ClosedInterval.cpp
@@ -0,0 +1,11 @@
+// Problem Link - https://atcoder.jp/contests/abc454/tasks/abc454_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int l,r;
+    cin >> l >> r;
+    cout << r-l+1 << endl;
+    return 0;
+}
