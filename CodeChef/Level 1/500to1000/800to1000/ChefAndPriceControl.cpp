4515a381514be0ebfd1ae6099c40c7065faab48e
Swastik555 <swastikgondhi2204@gmail.com>
2026-04-20 22:56:04 +0530

    Update ChefAndPriceControl.cpp

--- CodeChef/Level 1/500to1000/800to1000/ChefAndPriceControl.cpp
+++ CodeChef/Level 1/500to1000/800to1000/ChefAndPriceControl.cpp
@@ -1,2 +1,22 @@
 // Problem Link - https://www.codechef.com/practice/course/logical-problems/DIFF1000/problems/PRICECON
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+        int loss = 0;
+        for(int i=1;i<=n;i++){
+            int x;
+            cin >> x;
+            if(x>k) loss += (x-k);
+        }
+        
+        cout << loss << endl;
+    }
+}
