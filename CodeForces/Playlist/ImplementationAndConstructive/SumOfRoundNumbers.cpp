3db2147ae43b366440bbb3fb26fe4c3dc7dc14a1
Swastik555 <swastikgondhi2204@gmail.com>
2026-04-24 23:49:08 +0530

    Create SumOfRoundNumbers.cpp
    24 April 2026
    Radhe Radhe

---
+++ CodeForces/Playlist/ImplementationAndConstructive/SumOfRoundNumbers.cpp
@@ -0,0 +1,48 @@
+// Problem Link - https://codeforces.com/problemset/problem/1352/A
+// important constraint - 1 <= n <= 10,000
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k=0;
+        cin >> n;
+        vector<int> v;
+        if(n==10000){
+            cout << 1 << endl << 10000 << endl;
+        }
+        else {
+            while(n!=0){
+                if(n%10!=0){
+                    k++;
+                    v.push_back(n%10);
+                    n-=n%10;
+                }
+                if(n%100!=0){
+                    k++;
+                    v.push_back(n%100);
+                    n-=n%100;
+                }
+                if(n%1000!=0){
+                    k++;
+                    v.push_back(n%1000);
+                    n-=n%1000;
+                }
+                if(n%10000!=0){
+                    k++;
+                    v.push_back(n%10000);
+                    n-=n%10000;
+                }
+            }
+            cout << k << endl;
+            for(int i=0;i<v.size();i++){
+                cout << v[i] << " ";
+            }
+            cout << endl;
+            }
+    }
+    return 0;
+}
