156a1a4e86b5ec607095a46b38b38fe513cfd75e
Swastik555 <swastikgondhi2204@gmail.com>
2026-04-28 17:47:19 +0530

    Update Problem.cpp

--- CodeChef/Level 1/500to1000/800to1000/Problem.cpp
+++ CodeChef/Level 1/500to1000/800to1000/Problem.cpp
@@ -1,2 +1,28 @@
 // Problem Link - https://www.codechef.com/practice/course/logical-problems/DIFF1000/problems/PR0BLEM
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        if(n<m){
+            while(n<m){
+                m--;
+                n+=3;
+            }
+        }
+        if(n>m){
+            while(n>m){
+                n--;
+                m++;
+            }
+        }
+        if(n==m) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
