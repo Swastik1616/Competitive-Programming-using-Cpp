e128a91ae0f9ab636d35ab259293a93601921d18
Swastik555 <swastikgondhi2204@gmail.com>
2026-04-29 20:06:24 +0530

    Create ExtraGuests.cpp
    29 April 2026
    Radhe Radhe

---
+++ CodeChef/Contests/29-April-2026/ExtraGuests.cpp
@@ -0,0 +1,11 @@
+// Problem Link - https://www.codechef.com/START236D/problems/EXGS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin >> x >> y;
+    cout << 100*x + 150*(y-x) << endl;
+}
