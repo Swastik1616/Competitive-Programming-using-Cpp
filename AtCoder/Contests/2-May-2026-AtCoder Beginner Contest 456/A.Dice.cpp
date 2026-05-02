cde66d3ef2d2f63902493e2431ce7b04b033fc55
Swastik555 <swastikgondhi2204@gmail.com>
2026-05-02 17:49:52 +0530

    Create A.Dice.cpp
    2 May 2026
    Radhe Radhe

---
+++ AtCoder/Contests/2-May-2026-AtCoder Beginner Contest 456/A.Dice.cpp
@@ -0,0 +1,12 @@
+// Problem Link - 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n==1 || n==2 || n==19 || n==20) cout << "No\n";
+    else cout << "Yes\n";
+    return 0;
+}
