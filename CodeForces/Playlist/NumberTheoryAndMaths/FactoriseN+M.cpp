8b776e5fca34bf8ed596aa4275276211d068ecac
Swastik555 <swastikgondhi2204@gmail.com>
2026-05-03 20:20:22 +0530

    Create FactoriseN+M.cpp
    3 May 2026
    Radhe Radhe

---
+++ CodeForces/Playlist/NumberTheoryAndMaths/FactoriseN+M.cpp
@@ -0,0 +1,20 @@
+// Problem Link - https://codeforces.com/problemset/problem/1740/A
+/* 2 is the only even prime, in rest primes, add any odd primes, 
+the result will always be non prime, so simply we can add any prime lets just add 7 to all 
+and thats it*/
+
+/* 2+7,3+7,5+7,7+7,11+7....see all results are non prime*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << 7 << endl;
+    }
+    return 0;
+}
