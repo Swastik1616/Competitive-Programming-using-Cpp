8398ee2e5faed8745d0f278e521a16fd8c6da033
Swastik555 <swastikgondhi2204@gmail.com>
2026-05-08 14:38:36 +0530

    Create GCDandLCM.cpp
    8 May 2026
    Radhe Radhe

---
+++ CodeForces/Playlist/NumberTheoryAndMaths/GCDandLCM.cpp
@@ -0,0 +1,18 @@
+// Problem Link - https://codeforces.com/problemset/problem/1665/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << n-3 << " " << 1 << " " << 1 << " " << 1 << "\n";
+    }
+    return 0;
+}
