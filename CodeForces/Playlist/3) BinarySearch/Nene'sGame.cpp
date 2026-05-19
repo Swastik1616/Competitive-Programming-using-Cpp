c7ce28bf115f2a67dedd189cf35a1c400d585f56
Swastik555 <swastikgondhi2204@gmail.com>
2026-05-19 22:18:14 +0530

    Create Nene'sGame.cpp
    19 May 2026
    Radhe Radhe

---
+++ CodeForces/Playlist/3) BinarySearch/Nene'sGame.cpp
@@ -0,0 +1,12 @@
+// Problem Link - https://codeforces.com/contest/1956/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    return 0;
+}
