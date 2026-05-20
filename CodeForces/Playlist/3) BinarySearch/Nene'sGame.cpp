3b697fad6f408f26642f69ff501d006eb88c0054
Swastik555 <swastikgondhi2204@gmail.com>
2026-05-20 23:25:35 +0530

    Update Nene'sGame.cpp
    20 May 2026
    Radhe Radhe

--- CodeForces/Playlist/3) BinarySearch/Nene'sGame.cpp
+++ CodeForces/Playlist/3) BinarySearch/Nene'sGame.cpp
@@ -1,5 +1,5 @@
 // Problem Link - https://codeforces.com/contest/1956/problem/A
-
+// crazyyy
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,6 +7,25 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int t;
+    cin >> t;
+    while(t--){
+        int k,q;
+        cin >> k >> q;
+        vector<int> a(k),b(q);
+        for(int i=0;i<k;i++){
+            cin >> a[i];
+        }
+        for(int i=0;i<q;i++){
+            cin >> b[i];
+        }
+
+        int x = a[0] - 1;
+        for(auto it:b){
+            if(it<a[0]) cout << it << " ";
+            else cout << x << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
