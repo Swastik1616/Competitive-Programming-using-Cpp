f17d5dadf3b04d0cb433bb091333e0919d47f6c6
Swastik555 <swastikgondhi2204@gmail.com>
2026-06-10 21:01:57 +0530

    Create SeatedTogether.cpp
    10 June 2026
    Radhe Radhe

---
+++ CodeChef/Contests/2026/3) June/10-June-2026/SeatedTogether.cpp
@@ -0,0 +1,12 @@
+// Problem Link - https://www.codechef.com/START242D/problems/SAMESIT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if(x%5==0) cout << "NO\n";
+    else cout << "YES\n";
+}
