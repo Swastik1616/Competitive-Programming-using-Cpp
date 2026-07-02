48cb68eba8181e28926d896a8ff516796bd99ea9
Swastik555 <swastikgondhi2204@gmail.com>
2026-07-02 17:01:49 +0530

    Update 40) DivideAndConquer.cpp

--- CodeForces/CP-Training-Sheet/A/40) DivideAndConquer.cpp
+++ CodeForces/CP-Training-Sheet/A/40) DivideAndConquer.cpp
@@ -1,23 +1,28 @@
-// Problem Link - https://codeforces.com/contest/2241/problem/A
 
+// Problem Link - https://codeforces.com/contest/2241/problem/A
+ 
 #include<bits/stdc++.h>
 #define ff first
 #define ss second
 using namespace std;
 using ll = long long;
 const ll MOD = 1e9 + 7;
-
+ 
+void solve(){
+        int a,b;
+        cin >> a >> b;
+        if(a%b==0) cout << "YES\n";
+        else cout << "NO\n";
+}
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+ 
     int t;
     cin >> t;
     while(t--){
-        int x,y;
-        cin >> x >> y;
-        if(x%y==0) cout << "YES\n";
-        else cout << "NO\n";
+        solve();
     }
     
     return 0;
