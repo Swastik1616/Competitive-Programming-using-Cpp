91270fbd7e91ba987f185da08e40a1712b654b4c
Swastik555 <swastikgondhi2204@gmail.com>
2026-07-07 21:38:22 +0530

    Create 5) JaggedSwaps.cpp
    7 July 2026
    Radhe Radhe

---
+++ TLE-Eliminators/CP-31-Sheets/800-Rated/5) JaggedSwaps.cpp
@@ -0,0 +1,34 @@
+// Problem Link - https://codeforces.com/problemset/problem/1896/A
+
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &it:v) cin >> it;
+    if(v[0]==1) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+void TCs(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TCs();
+    //solve();
+    return 0;
+}
