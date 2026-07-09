61d2cda1aedb94a98be56f406ffeed10464528b4
Swastik555 <swastikgondhi2204@gmail.com>
2026-07-09 19:57:48 +0530

    Create 1) XORwice.cpp
    9 July 2026
    Radhe Radhe

---
+++ TLE-Eliminators/Courses/Level-2/Module2-BitManipulationBeginner/Practice Problems/1) XORwice.cpp
@@ -0,0 +1,33 @@
+// Problem Link - https://codeforces.com/problemset/problem/1421/A
+
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+    int a,b;
+    cin >> a >> b;
+    int ans = a^b;
+    cout << ans << "\n";
+}
+
+void TCs(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TCs();
+    //solve();
+    
+    return 0;
+}
