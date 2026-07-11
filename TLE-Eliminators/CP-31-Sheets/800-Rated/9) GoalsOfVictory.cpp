a6eb2fdf79dbc17f0374e31dc895443bb3d2217b
Swastik555 <swastikgondhi2204@gmail.com>
2026-07-11 22:16:38 +0530

    Create 9) GoalsOfVictory.cpp
    11 July 2026
    Radhe Radhe

---
+++ TLE-Eliminators/CP-31-Sheets/800-Rated/9) GoalsOfVictory.cpp
@@ -0,0 +1,38 @@
+// Problem Link - 
+
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin >> n;
+    int ans = 0;
+    for(int i=0;i<n-1;i++){
+        int x;
+        cin >> x;
+        ans += x;
+    }
+    cout << (-1)*ans << "\n";
+}
+
+void TCs(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TCs();
+    //solve();
+    
+    return 0;
+}
