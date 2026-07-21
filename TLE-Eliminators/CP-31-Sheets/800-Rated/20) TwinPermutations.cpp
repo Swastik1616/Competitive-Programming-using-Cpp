ccc19ce1d74c471b8c7d7a9be1c6f28300da2f1a
Swastik555 <swastikgondhi2204@gmail.com>
2026-07-21 21:54:33 +0530

    Create 20) TwinPermutations.cpp
    21 July 2026
    Radhe Radhe

---
+++ TLE-Eliminators/CP-31-Sheets/800-Rated/20) TwinPermutations.cpp
@@ -0,0 +1,35 @@
+// Problem Link - 
+
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &it:v) cin >> it;
+    for(int i=0;i<n;i++) cout << n + 1 - v[i] << " ";
+    cout << "\n";
+}
+
+void TCs(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TCs();
+    //solve();
+    
+    return 0;
+}
