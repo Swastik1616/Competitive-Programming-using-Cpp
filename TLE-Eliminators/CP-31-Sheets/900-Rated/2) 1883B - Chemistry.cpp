00bfaed50b09620ba2f751ed5ff415afd9ff2225
Swastik555 <swastikgondhi2204@gmail.com>
2026-08-02 23:54:09 +0530

    Create 2) 1883B - Chemistry.cpp
    2 August 2026
    Radhe Radhe

---
+++ TLE-Eliminators/CP-31-Sheets/900-Rated/2) 1883B - Chemistry.cpp
@@ -0,0 +1,44 @@
+// Problem Link - https://codeforces.com/problemset/problem/1883/B
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<ll>
+#define vin for(auto &it:v) cin >> it
+
+void solve(){
+    int n,k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    map<char,int> mp;
+    for(auto it:s) mp[it]++;
+    int c = 0;
+    for(auto it:mp) if(it.ss&1) c++;
+    if(c>k+1) cout << "NO\n";
+    else cout << "YES\n";
+}
+
+void TCs(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TCs();
+    //solve();
+    
+    return 0;
+}
