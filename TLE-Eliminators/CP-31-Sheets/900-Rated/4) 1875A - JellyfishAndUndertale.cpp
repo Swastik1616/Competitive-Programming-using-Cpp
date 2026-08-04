d9e509a06dea4e6e9af8d6449c164e386fbc6f9f
Swastik555 <swastikgondhi2204@gmail.com>
2026-08-04 22:46:02 +0530

    Create 4) 1875A - JellyfishAndUndertale.cpp
    4 August 2026
    Radhe Radhe

---
+++ TLE-Eliminators/CP-31-Sheets/900-Rated/4) 1875A - JellyfishAndUndertale.cpp
@@ -0,0 +1,40 @@
+// Problem Link - https://codeforces.com/problemset/problem/1875/A
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<ll>
+#define vin for(auto &it:v) cin >> it
+
+void solve(){   
+    ll a,b,n;
+    cin >> a >> b >> n;
+    vll v(n);
+    vin;
+    for(auto it:v) b += min(it,a-1);
+    cout << b << "\n";
+}
+
+void TCs(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TCs();
+    //solve();
+    
+    return 0;
+}
