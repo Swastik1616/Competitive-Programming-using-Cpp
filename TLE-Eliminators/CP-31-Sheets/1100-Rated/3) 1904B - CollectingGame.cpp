a4dc543e868b7154f0e3a408a65d58a7aea8dfc2
Swastik555 <swastikgondhi2204@gmail.com>
2026-08-30 23:11:51 +0530

    Create 3) 1904B - CollectingGame.cpp
    will do tomorrow
    Radhe Radhe

---
+++ TLE-Eliminators/CP-31-Sheets/1100-Rated/3) 1904B - CollectingGame.cpp
@@ -0,0 +1,35 @@
+// Problem Link - https://codeforces.com/problemset/problem/1904/B
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<ll>
+#define vpi vector<pair<int,int>> 
+
+void solve(){
+
+}
+
+void TCs(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    TCs();
+    //solve();
+    
+    return 0;
+}
