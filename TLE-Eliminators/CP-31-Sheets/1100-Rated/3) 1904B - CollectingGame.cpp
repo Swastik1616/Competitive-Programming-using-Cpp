b2646484a162ab489861134370f80c9d764805e5
Swastik555 <swastikgondhi2204@gmail.com>
2026-08-31 20:52:13 +0530

    Update 3) 1904B - CollectingGame.cpp
    31 August 2026
    Radhe Radhe

--- TLE-Eliminators/CP-31-Sheets/1100-Rated/3) 1904B - CollectingGame.cpp
+++ TLE-Eliminators/CP-31-Sheets/1100-Rated/3) 1904B - CollectingGame.cpp
@@ -11,9 +11,46 @@ const ll MOD = 1e9 + 7;
 #define vi vector<int>
 #define vll vector<ll>
 #define vpi vector<pair<int,int>> 
+#define vpll vector<pair<ll,ll>>
 
 void solve(){
+        ll n; 
+		cin >> n;
+		vpll v; 
 
+		for(int i=0;i<n;i++){
+            ll x;
+			cin >> x;
+			v.push_back({x, i});
+		}
+
+		vll pre(n);
+		sort(v.begin(), v.end());
+
+		pre[0] = v[0].ff;
+		for(int i=1;i<n;i++){
+			pre[i] = pre[i - 1] + v[i].ff;
+		}
+
+		vll ans(n);
+		for (int i=0;i<n;i++){ 
+			int j = i;
+			int found = i;
+			while (j < n) {
+				pair<ll, ll> temp = {pre[j] + 1, INT_MIN};
+				ll idx = lower_bound(v.begin(), v.end(), temp) - v.begin();
+				idx--;
+				if (idx == j) {
+					break;
+				}
+				found += idx - j;
+				j = idx;
+			}
+			ans[v[i].ss] = found;
+		}
+
+        for(auto it:ans) cout << it << " ";
+		cout << "\n";    
 }
 
 void TCs(){
